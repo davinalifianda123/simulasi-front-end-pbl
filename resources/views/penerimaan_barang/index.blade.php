0366e3edb37d5f735147aecff2e9f6a366ac3d60
Davin <dav1nalifianda@gmail.com>
2025-06-17 22:23:50 +0700

    tambah checklist diterima

--- resources/views/penerimaan_barang/index.blade.php
+++ resources/views/penerimaan_barang/index.blade.php
@@ -29,6 +29,9 @@
                                 </span>
                             </th>
                             @endforeach
+                            <th>
+                                <span class="flex items-center">Diterima</span>
+                            </th>
                         @endif
                     </tr>
                 </thead>
@@ -41,6 +44,9 @@
                             <td>{{ $penerimaanDiPusat->jumlah_barang }}</td>
                             <td>{{ $penerimaanDiPusat->tanggal }}</td>
                             <td>{{ $penerimaanDiPusat->jenis_penerimaan }}</td>
+                            <td class="text-center">
+                                <input id="default-checkbox" type="checkbox" name="diterima[]" value="{{ $penerimaanDiPusat->id }}" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded-sm focus:ring-blue-500 dark:focus:ring-blue-600">
+                            </td>
                         </tr>
                     @empty
                         <tr>
