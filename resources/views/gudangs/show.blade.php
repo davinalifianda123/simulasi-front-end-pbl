1d0bf04df81913d1d7f9f3f04a26b0bcb5da355e
Davin <dav1nalifianda@gmail.com>
2025-07-06 12:17:40 +0700

    perbaikan minor

--- resources/views/gudangs/show.blade.php
+++ resources/views/gudangs/show.blade.php
@@ -40,9 +40,7 @@
                     <div class="bg-gray-50 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
                         <dt class="text-sm font-medium text-gray-500">Status</dt>
                         <dd class="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
-                            <span class="px-2 inline-flex text-xs leading-5 font-semibold rounded-full bg-gray-100 text-gray-800">
-                                {{ $gudang->status }}
-                            </span>
+                            {!! $gudang->status == 'Aktif' ? '<span class="bg-green-100 text-green-800 text-xs font-medium me-2 px-2.5 py-0.5 rounded-full">Aktif</span>' : '<span class="bg-red-100 text-red-800 text-xs font-medium me-2 px-2.5 py-0.5 rounded-full">Nonaktif</span>' !!}
                         </dd>
                     </div>
                 </dl>
